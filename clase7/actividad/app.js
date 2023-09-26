8c1b38ea162ce2ebc77d6d088c539a1731d27d6c
JuanPabloRP <jpabloroldanp@gmail.com>
2023-09-25 22:55:22 -0500

    feat: actividad DOM

---
+++ clase7/actividad/app.js
@@ -0,0 +1,10 @@
+const algoXD = document.getElementById('algunaChimbadaXD');
+
+const infoSinSentido = ['anasheee', 'douu', 'XDd'];
+
+infoSinSentido.forEach((i) => {
+	const elem = (i.createElement = document.createElement('p'));
+	elem.className = 'textoXD';
+	elem.innerHTML = i;
+	algoXD.append(elem);
+});
