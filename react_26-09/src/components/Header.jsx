75debb418aaa95f991d535d0805c6fa025ce4fc8
Juan Pablo Roldán Patiño <56945649+JuanPabloRP@users.noreply.github.com>
2023-10-09 23:54:58 +0000

    feat: cambios react_26_09

---
+++ react_26-09/src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+	return (
+		<header>
+			<a href=""></a>
+
+			<nav>
+				<u>
+					<li>prod 1</li>
+					<li>prod 2</li>
+				</u>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
